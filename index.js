edf81996e2ea56e50f4918355478a655caefb099
jeongseungmin <jsm8109jsm@gmail.com>
2023-08-26 20:58:07 +0900

    FEAT :: express 추가, 유저 정보 GET

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const readline = require("readline");
-const { BsmOauth, BsmOauthError, BsmOauthErrorType, BsmUserRole, BsmStudentResource, BsmTeacherResource } = require("bsm-oauth");
+require("dotenv").config(); // dotenv 설정
+const {
+  BsmOauth,
+  BsmOauthError,
+  BsmOauthErrorType,
+  BsmUserRole,
+  BsmStudentResource,
+  BsmTeacherResource,
+} = require("bsm-oauth");
 
+const express = require("express");
+const app = express();
+const port = 3000;
+let userInfo = {};
+
+app.get("/api/search", async (req, res) => {
+  await getHtml(req.query.nickname);
+  res.json(userInfo);
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at localhost:3000`);
+});
 
 const bsmOauth = new BsmOauth(
   process.env.BSM_AUTH_CLIENT_ID,
   process.env.BSM_AUTH_CLIENT_SECRET
 );
 
-console.log(bsmOauth);
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let input = "";
-
-rl.on("line", (line) => {
-  input = line;
-  rl.close();
-});
-
-rl.on("close", () => {
-  getHtml(input);
-});
+// console.log(process.env.BSM_AUTH_CLIENT_ID, process.env.BSM_AUTH_CLIENT_SECRET);
 
 const getHtml = async (nickname) => {
   const url = `https://maple.gg/u/${nickname}`;
   try {
     const html = await axios.get(url);
-    let user = {};
     const $ = cheerio.load(html.data);
     const bodyList = $("div.user-profile");
-    // console.log(bodyList)
     bodyList.map((i, element) => {
-      user = {
+      userInfo = {
         server: $(element)
           .find("div.user-detail li.user-summary-item:first-child")
           .text()
@@ -59,8 +62,8 @@ const getHtml = async (nickname) => {
 
     const muleng = $("div.col-lg-3.col-6.mt-3.px-1:first-child");
     muleng.map((i, element) => {
-      user = {
-        ...user,
+      userInfo = {
+        ...userInfo,
         muleng_floor: $(element)
           .find("h1.user-summary-floor.font-weight-bold")
           .text()
@@ -71,8 +74,8 @@ const getHtml = async (nickname) => {
           .replace(/\s/g, ""),
       };
     });
-    if (Object.keys(user).length !== 0) {
-      console.log("userInfo : ", user);
+    if (Object.keys(userInfo).length !== 0) {
+      console.log("userInfo : ", userInfo);
       return;
     }
     console.log("유저 정보가 없습니다");
